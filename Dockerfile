6c2883e1cf8f68e675e02de463b4aa7174028b42
Louis Nguyen <thonh.it@gmail.com>
2024-01-05 13:08:13 +0700

    update build

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM alpine:3.14
+FROM python:3.7.0
 
-COPY ./dist/app /app
+WORKDIR /app
+COPY . /app
 
-CMD ["/app"]
+RUN pip install -r requirements.txt
+CMD ["python", "app.py"]
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # pyinstaller -p . -w -F --add-data "templates:templates" --add-data "static:static" --clean app.py
 
-docker run --rm -v "${PWD}:/src" -e PYTHONHASHSEED=42 stevenacoffman/pyinstaller-alpine -p . -w -F --add-data "templates:templates" --add-data "static:static" app.py
+docker run --rm -v "${PWD}:/code" -e PYTHONHASHSEED=42 coopermaa/pyinstaller -p . -w -F --add-data "templates:templates" --add-data "static:static" app.py
